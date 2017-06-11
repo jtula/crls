9383cc37a0d3d05394a2cf906638644b36fc095f
Jose A Tula Leyva <joseph.tula@gmail.com>
2017-06-10 22:52:06 -0300

    insertion_sort

---
+++ insertion_sort.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+def insertion_sort(a):
+    '''
+    Insertion sort (CLRS - 3ed, pg 18).
+    Time complexity: O(n^2)
+    Space complexity: O(1)
+    '''
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j - 1
+        while i >= 0 and a[i] > key:
+            a[i + 1] = a[i]
+            i = i - 1
+        a[i + 1] = key
+
+
+def main():
+    a = [5, 2, 4, 6, 1, 3]    
+    print('Array: ', a)
+    insertion_sort(a)
+    print('Insertion sort: ', a)
+
+
+
+if __name__=="__main__":
+    main()
